9902ca69a4133e8fa8a3249a85c985c4894bb63a
unknown <jissgeorge55@gmail.com>
2021-01-23 14:53:07 +0530

    Configured Sauce Labs

--- YourLogoRegressionAutomationTest/src/test/java/selenium/framework/testbase/TestBase.java
+++ YourLogoRegressionAutomationTest/src/test/java/selenium/framework/testbase/TestBase.java
@@ -3,8 +3,10 @@ package selenium.framework.testbase;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -14,6 +16,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -33,10 +37,14 @@ public class TestBase {
 	static ReadConfigFile readconfig = new ReadConfigFile("config.properties");
 	public SeleniumElements elements = new SeleniumElements(driver);
 	
+	public static final String USERNAMR = "Jissg";
+	public static final String ACCESS_KEY = "e8a2c70c-c904-4477-bf9c-18b54077d4ca";
+	public static final String URL ="https://"+USERNAMR+":"+ACCESS_KEY+"@ondemand.us-west-1.saucelabs.com:443/wd/hub";
 	
-	@Parameters("browser")
+	@Parameters({"browser", "runtype", "version", "platform"})
 	@BeforeClass(alwaysRun=true)
-	public static void setup(String br)
+	public static void setup(String br, String type, String vr, String pf)
+	
 	{
 		TestBase base = new TestBase();
 		
@@ -45,6 +53,8 @@ public class TestBase {
 		String directory= System.getProperty("user.dir");
 		String browser = readconfig.getbrowser();
 		
+		if(type.equals("normal"))
+		{
 		if(br.equals("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver", directory+"\\Drivers\\chromedriver.exe");
@@ -66,7 +76,24 @@ public class TestBase {
 		else {
 			System.out.println("Unknown browser");
 		}
+		}
 		
+		else if(type.equalsIgnoreCase("saucelabs"))
+		{
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability("browserName", br);
+			cap.setCapability("version", vr);
+			cap.setCapability("platform", pf);
+			
+			
+			try {
+			driver = new RemoteWebDriver(new URL(URL), cap);
+			}catch(MalformedURLException e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(utilities.pageLoadTimeinSec, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(utilities.implicitWaitinSec, TimeUnit.SECONDS);
